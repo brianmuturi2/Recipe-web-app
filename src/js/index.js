ab30942f5a21462464522fc2199a5f5610d7bd5b
brianmuturi2 <brianmuturi2@gmail.com>
2019-10-03 19:02:44 +0300

    First api call

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,20 @@
-// Global app controller
-import num from './test';
-const x = 23;
-console.log(`I imported ${num} from another module called test.js! Variable x is ${x}`);
+// https://www.food2fork.com/api/search
 
+//  API Key: 4d66164e4dbd8f0388ed1b57d333a238
+
+import axios from 'axios';
+
+async function getResults(query){
+
+    const myApi = `https://www.food2fork.com/api/search`;
+    const key =`4d66164e4dbd8f0388ed1b57d333a238`;
+    try {
+
+        const res = await axios(`${myApi}?key=${key}&q=${query}`);
+        const recipes = res.data.recipes;
+        console.log(recipes);
+    } catch(error) {
+        alert(error);
+    }
+}
+getResults(`pizza`);
