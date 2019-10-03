9aac8742cfd40237b40f9e433ff762f18da27255
brianmuturi2 <brianmuturi2@gmail.com>
2019-10-03 19:15:07 +0300

    Building search model

--- src/js/index.js
+++ src/js/index.js
@@ -2,19 +2,8 @@
 
 //  API Key: 4d66164e4dbd8f0388ed1b57d333a238
 
-import axios from 'axios';
+import Search from './models/Search';
 
-async function getResults(query){
-
-    const myApi = `https://www.food2fork.com/api/search`;
-    const key =`4d66164e4dbd8f0388ed1b57d333a238`;
-    try {
-
-        const res = await axios(`${myApi}?key=${key}&q=${query}`);
-        const recipes = res.data.recipes;
-        console.log(recipes);
-    } catch(error) {
-        alert(error);
-    }
-}
-getResults(`pizza`);
+const search = new Search(`pizza`);
+console.log(search);
+search.getResults();
--- src/js/models/Search.js
+++ src/js/models/Search.js
@@ -1,1 +1,22 @@
-export default 'Iam an exported string';
+import axios from 'axios';
+
+export default class Search {
+    constructor(query){
+        this.query = query;
+    }
+
+    async getResults(){
+
+        const myApi = `https://www.food2fork.com/api/search`;
+        const key =`4d66164e4dbd8f0388ed1b57d333a238`;
+        try {
+    
+            const res = await axios(`${myApi}?key=${key}&q=${this.query}`);
+            this.result = res.data.recipes;
+            console.log(this.result);
+        } catch(error) {
+            alert(error);
+        }
+    }
+
+}
